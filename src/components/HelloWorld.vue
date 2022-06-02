56a1b1b7e013f1da99347917afae7bd2345b0d26
Christopher Debray <christopher.debray@club-employes.com>
2022-06-02 11:25:45 +0200

    INIT project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div>
+    Hello
+  </div>
+</template>
+
+<style scoped>
+
+</style>
